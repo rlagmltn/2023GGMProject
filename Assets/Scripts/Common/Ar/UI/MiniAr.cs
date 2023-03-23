4412f930a9d9c4dd5489b7a3a6585772ed79061c
zmflqhaos <zmflqhaos123@gmail.com>
2023-03-23 11:45:30 +0900

    미니맵에 공격거리

--- Assets/Scripts/Common/Ar/UI/MiniAr.cs
+++ Assets/Scripts/Common/Ar/UI/MiniAr.cs
@@ -7,6 +7,9 @@ public class MiniAr : MonoBehaviour
 {
     private Ar targetAr;
     private Image image;
+    private RectTransform range;
+
+    private Transform activeRange;
 
     private void Update()
     {
@@ -17,15 +20,34 @@ public class MiniAr : MonoBehaviour
     {
         if (!targetAr.gameObject.activeSelf) gameObject.SetActive(false);
         transform.localPosition = targetAr.transform.position*MiniMap.Instance.GetMiniMapRatio();
+        if(activeRange!=null) transform.rotation = activeRange.rotation;
     }
 
     public void SetTarget(Ar ar)
     {
         targetAr = ar;
         image = GetComponent<Image>();
+        range = transform.GetChild(0).GetComponent<RectTransform>();
+        range.gameObject.SetActive(false);
 
         if (targetAr.CompareTag("Player")) image.color = Color.blue;
         else if (targetAr.CompareTag("Enemy")) image.color = Color.red;
         else image.color = Color.black;
+
+        targetAr.GetComponent<Player>()?.SetMini(this);
+    }
+
+    public void ShowRange(GameObject obj)
+    {
+        range.gameObject.SetActive(true);
+        range.sizeDelta = obj.transform.localScale * 50;
+        range.transform.localPosition = obj.transform.localPosition * 50;
+        activeRange = obj.transform;
+    }
+
+    public void DisableRange()
+    {
+        range.gameObject.SetActive(false);
+        activeRange = null;
     }
 }
