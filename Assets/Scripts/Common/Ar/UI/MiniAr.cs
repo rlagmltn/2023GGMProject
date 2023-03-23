151a48ee9e2ecdbc12537edb1d7f5e97d88e9c03
zmflqhaos <zmflqhaos123@gmail.com>
2023-03-23 11:45:49 +0900

    거리 수정

--- Assets/Scripts/Common/Ar/UI/MiniAr.cs
+++ Assets/Scripts/Common/Ar/UI/MiniAr.cs
@@ -40,8 +40,8 @@ public class MiniAr : MonoBehaviour
     public void ShowRange(GameObject obj)
     {
         range.gameObject.SetActive(true);
-        range.sizeDelta = obj.transform.localScale * 50;
-        range.transform.localPosition = obj.transform.localPosition * 50;
+        range.sizeDelta = obj.transform.localScale * 30;
+        range.transform.localPosition = obj.transform.localPosition * 30;
         activeRange = obj.transform;
     }
 
