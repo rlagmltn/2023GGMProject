a57d63d7eea2f2812204b408d8460e1e8b99127a
zmflqhaos <zmflqhaos123@gmail.com>
2023-05-08 19:38:53 +0900

    프리팹 되살리기

--- Assets/Scripts/Common/Ar/Game/BatchFieldChaser.cs
+++ Assets/Scripts/Common/Ar/Game/BatchFieldChaser.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class BatchFieldChaser : MonoBehaviour
 {
     private PlayerController playerController;
+    private CameraMove cameraMove;
+    private BoxCollider2D box;
     [SerializeField] Transform chase;
 
     private void Start()
     {
         playerController = FindObjectOfType<PlayerController>();
+        cameraMove = FindObjectOfType<CameraMove>();
+        box = GetComponent<BoxCollider2D>();
     }
 
     private void Update()
     {
         transform.position = chase.position;
-        if (!playerController.IsBatchMode) gameObject.SetActive(false);
+        ChangeSize(cameraMove.OrthographicSize);
+        if (!playerController.IsBatchMode) Destroy(gameObject);
+    }
+
+    private void ChangeSize(float size)
+    {
+        box.size = new Vector2(3 + 3 * size, 0.5f + 0.5f * size);
     }
 }
