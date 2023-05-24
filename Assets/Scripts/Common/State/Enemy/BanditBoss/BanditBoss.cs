c28abb7b347e29a2f471d3f72917b318b12d9761
paul050210 <leepaul050210@gmail.com>
2023-05-24 12:35:15 +0900

    방어력스킬들

--- Assets/Scripts/Common/State/Enemy/BanditBoss/BanditBoss.cs
+++ Assets/Scripts/Common/State/Enemy/BanditBoss/BanditBoss.cs
@@ -6,13 +6,21 @@ public class BanditBoss : Enemy
 {
     private int adSp;
     private float adWeight;
+    
     private bool pHeavyArmor;
     private int pOverload;
+
     private int passiveCnt;
+
+    private int overloadSp;
+    private float overloadWeight;
+
+    private bool pShield;
+    private int shieldCnt;
 
     protected override void Start()
     {
-        
+        base.Start();
     }
 
     public override void StatReset()
@@ -20,26 +28,49 @@ public class BanditBoss : Enemy
         adSp = 0;
         adWeight = 0f;
         pHeavyArmor = false;
+        pShield = false;
         pOverload = 0;
         passiveCnt = 4;
+        overloadSp = 0;
+        overloadWeight = 0f;
         
     }
 
     public void Passive()
     {
-        passiveCnt--;
-        if(passiveCnt == 0)
+        if(pOverload > 0)
         {
-            stat.SP += 7;
-            adSp += 7;
+            pOverload--;
+            if(pOverload == 0)
+            {
+                stat.SP = overloadSp;
+                stat.WEIGHT = overloadWeight;
+                overloadSp = 0;
+                overloadWeight = 0f;
+            }
         }
-        if(pHeavyArmor)
+        else if(shieldCnt > 0)
         {
-            HeavyArmor();
+            shieldCnt--;
+            stat.HP = Mathf.Min(stat.MaxHP, stat.HP + 10);
+            stat.ATK += 3;
+            pShield = true;
         }
-
-        stat.WEIGHT += adSp / 5 * 2;
-        adWeight += adSp / 5 * 2;
+        else
+        {
+            passiveCnt--;
+            if(pHeavyArmor)
+            {
+                HeavyArmor();
+            }
+            if(passiveCnt == 0)
+            {
+                stat.SP += 7;
+                adSp += 7;
+                stat.WEIGHT += adSp / 5 * 2;
+                adWeight += adSp / 5 * 2;
+            }
+        }
     }
 
     public void SkillHeavyArmor()
@@ -60,5 +91,22 @@ public class BanditBoss : Enemy
     public void SkillOverload()
     {
         pOverload = 2;
+        overloadSp = stat.SP;
+        overloadWeight = stat.WEIGHT;
+        stat.SP = 25;
+    }
+
+    public bool CanMove()
+    {
+        if (pOverload > 0 || shieldCnt > 0) return false;
+
+        return true;
+    }
+
+    public void SkillShield()
+    {
+        overloadSp = stat.SP;
+        stat.SP = 10;
+        shieldCnt = 1;
     }
 }
