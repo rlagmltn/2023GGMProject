853f0c1cf3c57f1edd1bab729d8ebcb2b5de9a4e
zmflqhaos <zmflqhaos123@gmail.com>
2023-06-08 12:15:41 +0900

    세이브함수분리

--- Assets/Scripts/Save/SaveDatas.cs
+++ Assets/Scripts/Save/SaveDatas.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class GameData
 {
     public int ClearStages;
     public int Gold;
 }
 
+[System.Serializable]
 public class PlayerData
 {
     public int UpgradePoint;
--- Assets/Scripts/Save/SaveManager.cs
+++ Assets/Scripts/Save/SaveManager.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class SaveManager : MonoSingleton<SaveManager>
 {
-    private PlayerData playerData;
-    private GameData gameData;
+    [SerializeField] private PlayerData playerData;
+    [SerializeField] private GameData gameData;
 
     private string SAVE_PATH = "";
     private string PLAYER_SAVE_FILENAME = "/PlayerSave.txt";
@@ -14,26 +14,37 @@ public class SaveManager : MonoSingleton<SaveManager>
 
     private void Awake()
     {
-        SAVE_PATH = Application.persistentDataPath + "/Save";
+        SAVE_PATH = Application.dataPath + "/Save";
         // Application.dataPath
         if (!Directory.Exists(SAVE_PATH))
         {
             Directory.CreateDirectory(SAVE_PATH);
         }
-        LoadFromJson();
+        PlayerDataLoad();
+        GameDataLoad();
     }
     private void Start()
     {
         InvokeRepeating("SaveToJson", 1f, 60f);
     }
 
-    private void LoadFromJson()
+    private void PlayerDataLoad()
     {
         if (File.Exists(SAVE_PATH + PLAYER_SAVE_FILENAME))
         {
             string json = File.ReadAllText(SAVE_PATH + PLAYER_SAVE_FILENAME);
             playerData = JsonUtility.FromJson<PlayerData>(json);
         }
+    }
+
+    private void PlayerDataSave()
+    {
+        string jsonPlayer = JsonUtility.ToJson(playerData, true);
+        File.WriteAllText(SAVE_PATH + PLAYER_SAVE_FILENAME, jsonPlayer, System.Text.Encoding.UTF8);
+    }
+
+    private void GameDataLoad()
+    {
         if (File.Exists(SAVE_PATH + GAME_SAVE_FILENAME))
         {
             string json = File.ReadAllText(SAVE_PATH + GAME_SAVE_FILENAME);
@@ -41,16 +52,15 @@ public class SaveManager : MonoSingleton<SaveManager>
         }
     }
 
-    private void SaveToJson()
+    private void GameDataSave()
     {
-        string jsonPlayer = JsonUtility.ToJson(playerData, true);
         string jsonGame = JsonUtility.ToJson(gameData, true);
-        File.WriteAllText(SAVE_PATH + PLAYER_SAVE_FILENAME, jsonPlayer, System.Text.Encoding.UTF8);
         File.WriteAllText(SAVE_PATH + GAME_SAVE_FILENAME, jsonGame, System.Text.Encoding.UTF8);
     }
 
     private void OnApplicationQuit()
     {
-        SaveToJson();
+        PlayerDataSave();
+        GameDataSave();
     }
 }
