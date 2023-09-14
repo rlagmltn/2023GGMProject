0cd1533a819c8d6a7d0a8d8cf6980d51d579a795
sg5404 <ggm.s210028@ggh.goe.go.kr>
2023-09-14 10:10:47 +0900

    사운드 매니저 관리

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -18,6 +18,20 @@ public class SoundManager : MonoSingleton<SoundManager>
     [SerializeField] Slider effectS;
     [SerializeField] Slider uiS;
 
+    [SerializeField] Transform Master_ON;
+    [SerializeField] Transform Master_OFF;
+    [SerializeField] Transform BGM_ON;
+    [SerializeField] Transform BGM_OFF;
+    [SerializeField] Transform Effect_ON;
+    [SerializeField] Transform Effect_OFF;
+    [SerializeField] Transform UI_ON;
+    [SerializeField] Transform UI_OFF;
+
+    public float MasterValue = 0;
+    public float BGMValue = 0;
+    public float EffectValue = 0;
+    public float UIValue = 0;
+
     private void Awake()
     {
         Init();
@@ -103,20 +117,124 @@ public class SoundManager : MonoSingleton<SoundManager>
     public void SetMasterVolume()
     {
         audioMixer.SetFloat("Master", Mathf.Log10(masterS.value) * 20);
+
+        if (masterS.value < 0.01)
+        {
+            Master_ON.gameObject.SetActive(false);
+            Master_OFF.gameObject.SetActive(true);
+        }
+        else
+        {
+            Master_ON.gameObject.SetActive(true);
+            Master_OFF.gameObject.SetActive(false);
+        }
+    }
+
+    public void MasterVolume_ButtonClick()
+    {
+        if(masterS.value > 0.01f)
+        {
+            MasterValue = masterS.value;
+            masterS.value = 0;
+        }
+        else
+        {
+            masterS.value = MasterValue;
+            MasterValue = 0;
+        }
+        SetMasterVolume();
     }
 
     public void SetBGMVolume()
     {
         audioMixer.SetFloat("BGM", Mathf.Log10(bgmS.value) * 20);
+        
+        if (bgmS.value < 0.01f)
+        {
+            BGM_ON.gameObject.SetActive(false);
+            BGM_OFF.gameObject.SetActive(true);
+        }
+        else
+        {
+            BGM_ON.gameObject.SetActive(true);
+            BGM_OFF.gameObject.SetActive(false);
+        }
+    }
+
+    public void BGMVolume_ButtonClick()
+    {
+        if (bgmS.value > 0.01)
+        {
+            BGMValue = bgmS.value;
+            bgmS.value = 0;
+        }
+        else
+        {
+            bgmS.value = BGMValue;
+            BGMValue = 0;
+        }
+        SetBGMVolume();
     }
 
     public void SetEffectVolume()
     {
         audioMixer.SetFloat("Effect", Mathf.Log10(effectS.value) * 20);
+
+        if (effectS.value < 0.01f)
+        {
+            Effect_ON.gameObject.SetActive(false);
+            Effect_OFF.gameObject.SetActive(true);
+        }
+        else
+        {
+            Effect_ON.gameObject.SetActive(true);
+            Effect_OFF.gameObject.SetActive(false);
+        }
+    }
+
+    public void EffectVolume_ButtonClick()
+    {
+        if (effectS.value > 0.01)
+        {
+            EffectValue = effectS.value;
+            effectS.value = 0;
+        }
+        else
+        {
+            effectS.value = EffectValue;
+            EffectValue = 0;
+        }
+        SetEffectVolume();
     }
 
     public void SetUIVolume()
     {
         audioMixer.SetFloat("UI", Mathf.Log10(uiS.value) * 20);
+
+        if (uiS.value < 0.01f)
+        {
+            UI_ON.gameObject.SetActive(false);
+            UI_OFF.gameObject.SetActive(true);
+        }
+        else
+        {
+            UI_ON.gameObject.SetActive(true);
+            UI_OFF.gameObject.SetActive(false);
+        }
+    }
+
+    public void UIVolume_ButtonClick()
+    {
+        if (uiS.value > 0.01)
+        {
+            UIValue = uiS.value;
+            uiS.value = 0;
+        }
+        else
+        {
+            uiS.value = UIValue;
+            UIValue = 0;
+        }
+        SetUIVolume();
     }
 }
